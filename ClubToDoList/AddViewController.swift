06ca17be03bd3fe8501ae01e8c5dc097cd19229a
wxxd-fxrest <fxrest704@gmail.com>
2024-03-20 11:04:11 +0900

    Add: 텍스트 입력 및 데이터 저장

--- ClubToDoList/AddViewController.swift
+++ ClubToDoList/AddViewController.swift
@@ -7,23 +7,43 @@
 
 import UIKit
 
-class AddViewController: ViewController {
+protocol AddViewControllerDelegate: AnyObject {
+    func didSaveTodoItem(_ todoItem: TodoItem)
+}
+
+class AddViewController: UIViewController, UITextFieldDelegate {
+    weak var delegate: AddViewControllerDelegate?
+    var todos: [TodoItem] = [] // 변경된 부분: 옵셔널이 아닌 일반 배열로 변경
+
+    @IBOutlet weak var textfield: UITextField!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        textfield.attributedPlaceholder = NSAttributedString(string: "오늘의 ToDo를 입력해 주세요.", attributes: [NSAttributedString.Key.foregroundColor : UIColor.gray])
+        
+        // 텍스트 필드의 delegate 설정
+        textfield.delegate = self
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func saveButtonTapped(_ sender: UIBarButtonItem) {
+        // Save 버튼을 눌렀을 때 실행되는 액션 메서드
+        guard let text = textfield.text, !text.isEmpty else {
+            return
+        }
+        
+        // 새로운 TodoItem 객체 생성
+        let todoItem = TodoItem(title: text)
+        
+        // todos 배열에 추가
+        todos.append(todoItem)
+        
+        // delegate를 통해 ViewController에 todoItem 전달
+        delegate?.didSaveTodoItem(todoItem)
+        print("todoItem \(todoItem)")
+        
+        // 이전 화면으로 돌아감
+        navigationController?.popViewController(animated: true)
     }
-    */
-
 }
+
--- ClubToDoList/ViewController.swift
+++ ClubToDoList/ViewController.swift
@@ -7,13 +7,55 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+struct TodoItem {
+    static var nextId = 1 // 다음에 추가될 아이템의 아이디
+    var id: Int
+    var title: String
+    var isCompleted: Bool
+
+    init(title: String, isCompleted: Bool = false) {
+        self.id = TodoItem.nextId
+        self.title = title
+        self.isCompleted = isCompleted
+        TodoItem.nextId += 1 // 아이디 증가
+    }
+}
+
+
+class ViewController: UIViewController, AddViewControllerDelegate {
+    // To-Do 데이터 배열
+    var todos: [TodoItem] = [] // 변경된 부분: 옵셔널이 아닌 일반 배열로 변경
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+                
+        // To-Do 배열이 비어 있는지 여부 확인
+        if todos.isEmpty {
+            print("투두 리스트는 비어 있습니다.")
+        } else {
+            print("투두 리스트 목록 \(todos)")
+        }
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        // 화면이 표시될 때마다 todos 배열을 출력하여 확인
+        print("현재 투두 리스트: \(todos)")
+    }
+ 
+
+    // AddViewControllerDelegate 프로토콜 메서드 구현
+    func didSaveTodoItem(_ todoItem: TodoItem) {
+        // To-Do 배열에 추가
+        todos.append(todoItem)
     }
 
-
+    // AddViewController를 호출하는 메서드
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if let addVC = segue.destination as? AddViewController {
+            addVC.delegate = self
+        }
+    }
+    
 }
-
